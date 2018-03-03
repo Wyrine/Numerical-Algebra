0f502d5c83c1e90010101f9ca259c02787e6c689
Wyrine <kashahat@gmail.com>
2018-03-03 18:04:54 -0500

    properly computing inverse hilbert

--- HilbertMatrix-Project2/hilbert.py
+++ HilbertMatrix-Project2/hilbert.py
@@ -13,17 +13,27 @@ def buildHilbert(n):
             hilbert[j, i] = hilbert[i, j] = 1/ (i + 1 + j + 1 - 1)
     return hilbert
 
-def binomialCoeff(n, r):
+def explicitInverse(hilbert, n):
+    hilbInv = np.array(hilbert)
+
+    for i in range(n):
+        for j in range(i, n):
+            hilbInv[j,i] = hilbInv[i, j] = (-1)**(i+j+2) * (i+j+1) \
+                    * binomialCoeff(n+i, n-j-1) * binomialCoeff(n+j, n-i-1)\
+                    * binomialCoeff(i+j, i)**2
+    return hilbInv
+
+def binomialCoeff(n, r, fac = {}):
     """
         Computes n choose r
     """
-    fac = {}
     nFact, rFact, nMinusRFact = factorial(n, fac), factorial(r, fac), factorial(n-r, fac)
     return nFact/(rFact * nMinusRFact)
 
 def factorial(x, fac):
     """
         computes x factorial
+        Note: any non-positive values return 1
     """
     if x in fac:
         return fac[x]
@@ -38,3 +48,4 @@ if __name__ == "__main__":
         sys.exit(1)
     n = int(sys.argv[1])
     hilbert = buildHilbert(n)
+    invHilb = explicitInverse(n)
