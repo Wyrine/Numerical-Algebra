96076e0146b7fb9817166eeaa7596a79b3da66d9
Wyrine <kashahat@gmail.com>
2018-03-03 14:49:16 -0500

    properly building an n sized hilbert matrix

--- HilbertMatrix-Project2/conditionNumber.py
+++ HilbertMatrix-Project2/conditionNumber.py
@@ -3,9 +3,16 @@
 import sys
 import numpy as np
 
+def buildHilbert(n):
+    hilbert = np.zeros((n,n), dtype = np.float64)
+    for i in range(n):
+        for j in range(i, n):
+            hilbert[j, i] = hilbert[i, j] = 1/ (i + 1 + j + 1 - 1)
+    return hilbert
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: ./conditionNumber.py hilbertDimensions")
         sys.exit(1)
     n = int(sys.argv[1])
+    hilbert = buildHilbert(n)
