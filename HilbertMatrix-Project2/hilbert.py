6322e8b571a7b1eb805e471fe0d10e4ca7940e6d
Wyrine <kashahat@gmail.com>
2018-03-05 15:35:12 -0500

    Properly solving system using LLT and QR
    :

--- HilbertMatrix-Project2/hilbert.py
+++ HilbertMatrix-Project2/hilbert.py
@@ -66,34 +66,32 @@ def factorial(x, fac):
     fac[x] = x * factorial(x - 1, fac)
     return fac[x]
 
-def choleski(A):
-    n = len(A)
-    l = np.zeros((n, n), dtype=np.float64)
-    
-    l[0, 0] = np.sqrt(A[0, 0])
-    for j in range(1, n):
-        l[j, 0] = A[j, 0] / l[0,0]
-
-
-    for i in range (1, n-1):
-        sub = 0
-        for k in range(i-1 ):
-            sub += (l[i, k] ** 2)
-        l[i, i] = np.sqrt(A[i, i] - sub)
-        
-        for j in range(i+1, n):
-            sub = 0
-            for k in range(i-1):
-                sub += (l[j, k] / l[i, k])
-            l[j, i] = (A[j, i] - sub) / l[i, i]
-
-    sub = 0
-    for k in range(n-1):
-        sub += (l[n-1, k] ** 2)
-    l[n-1, n-1] = np.sqrt(A[n-1, n-1] - sub)
-    return l
+#def choleski(A):
+#    n = len(A)
+#    l = np.zeros((n, n), dtype=np.float64)
+#    l[0, 0] = np.sqrt(A[0, 0])
+#    for j in range(1, n):
+#        l[j, 0] = A[j, 0] / l[0,0]
+#    for i in range (1, n-1):
+#        sub = 0
+#        for k in range(i-1 ):
+#            sub += (l[i, k] ** 2)
+#        l[i, i] = np.sqrt(A[i, i] - sub)
+#        for j in range(i+1, n):
+#            sub = 0
+#            for k in range(i-1):
+#                sub += (l[j, k] / l[i, k])
+#            l[j, i] = (A[j, i] - sub) / l[i, i]
+#    sub = 0
+#    for k in range(n-1):
+#        sub += (l[n-1, k] ** 2)
+#    l[n-1, n-1] = np.sqrt(A[n-1, n-1] - sub)
+#    return l
 
 def chol(a):
+    """
+        Choleski factorization of a = LL^T
+    """
     n = len(a)
 
     for k in range(n):
@@ -105,35 +103,71 @@ def chol(a):
         a[0:k, k] = 0.0
     return a
 
-def cholSolveSystem(L, b):
+def cholSolve(L, b):
+    """
+        Solves a system Ax = LL^Tx = b using forward and back substitution
+    """
+
     n = len(b)
-  # Solution of [L]{y} = {b}
-    for k in range(n):
-        b[k] = (b[k] - np.dot(L[k,0:k],b[0:k]))/L[k,k]
-  # Solution of [L_transpose]{x} = {y}
-    for k in range(n-1,-1,-1):
-        b[k] = (b[k] - np.dot(L[k+1:n,k],b[k+1:n]))/L[k,k]
+    b[0] = b[0]/ L[0,0]
+    #forward substitution
+    for i in range(1, n):
+        s = 0.0
+        for j in range(0, i):
+            s += L[i, j] * b[j]
+        b[i] = (b[i] - s) / L[i, i]
+
+    #back substitution
+    b[n-1] = b[n-1] / L[n-1, n-1]
+    for i in range(n-2, -1, -1):
+        s = 0.0
+        for j in range(i+1, n):
+            s += L[j, i] * b[j]
+        b[i] = (b[i] - s) / L[i][i]
     return b
 
+#def cholSolveSystem(L, b):
+#    n = len(b)
+#  # Solution of [L]{y} = {b}
+#    for k in range(n):
+#        b[k] = (b[k] - np.dot(L[k,0:k],b[0:k]))/L[k,k]
+#  # Solution of [L_transpose]{x} = {y}
+#    for k in range(n-1,-1,-1):
+#        b[k] = (b[k] - np.dot(L[k+1:n,k],b[k+1:n]))/L[k,k]
+#    return b
+
 def qrSolveSystem(q, r, b):
+    """
+        Solves a system Ax = QRx = b
+    """
     n = len(b)
-    #forward subst
-    for k in range(n):
-        b[k] = (b[k] - np.dot(q[k, 0:k], b[0:k])) / q[k,k]
-
-    for k in range(n-1, -1, -1):
-        b[k] = (b[k] - np.dot(r[k+1:n,k], b[k+1:n])) / r[k,k]
-
+    #Rx = Q^T * b
+    b = np.matmul(np.transpose(q), b)
+    #back substitution
+    b[n-1] = b[n-1] / r[n-1, n-1]
+    for i in range(n-2, -1, -1):
+        s = 0.0
+        for j in range(i+1, n):
+            s += r[i, j] * b[j]
+        b[i] = (b[i] - s) / r[i][i]
     return b
 
 def printMat(A):
+    """
+        Prints A in readable format
+    """
     n = len(A)
     for i in range(n):
+        print("\t", end="")
         for j in range(n):
-            print(A[i, j], end=" ")
-        print("\n")
+            print(A[i, j], end="  ")
+        print()
 
 def CompareMethods(hilbert):
+    """
+        Compares approximations of the inverse hilbert matrix using choleski and QR
+        to see which is more accurate
+    """
     n = len(hilbert)
     invHilb = explicitInverse(hilbert)
     apprInv = np.zeros((n,n), dtype=np.float64)
@@ -143,11 +177,11 @@ def CompareMethods(hilbert):
     print("Condition number with infinity norm:", getCondition(hilbert, invHilb, n))
 
     #Using LL^T
-    L = np.transpose(chol(np.array(hilbert)))
+    L = chol(np.array(hilbert))
     for i in range(n):
         e = np.zeros(n)
         e[i] = 1
-        apprInv[:, i] = cholSolveSystem(L, e)
+        apprInv[:, i] = cholSolve(L, e)
 
     print("H inverse using LLT:")
     printMat(apprInv)
