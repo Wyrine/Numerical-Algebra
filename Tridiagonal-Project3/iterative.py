80e302b871383bf7466a9a2f594fd040c8def7a5
Wyrine <kashahat@gmail.com>
2018-03-29 10:14:29 -0400

    fixed bug. Properly works.

--- Tridiagonal-Project3/iterative.py
+++ Tridiagonal-Project3/iterative.py
@@ -5,30 +5,29 @@
 # Math 472
 # March 29, 2018
 
-import sys
 import numpy as np
 from math import exp, log
 
 def jacobi(n, b, xReal, eps):
-		x = np.zeros(n, dtype=np.float64)
-		k, x2 = 0, np.zeros(n, dtype=np.float64)
+		x = np.zeros(n)
+		k, x2 = 0, np.zeros(n)
 		while True:
 				x2[0] = 1/2*(x[1] + b[0])
 				for i in range(1, n-1):
 						x2[i] = 1/2*(x[i-1] + x[i+1] + b[i])
 				x2[n-1] = 1/2*(x[n-2] + b[n-1])
 				k += 1
-				x = x2
+				x = np.array(x2)
 				infNorm = infinityNorm(x-xReal)
 				if infNorm < eps:
 						print("\tJacobi:")
 						print("\t\tK:", k)
-						print("\t\tSpectral Radius:", exp(1/k * log(infNorm/ infinityNorm(xReal), 2)))
+						print("\t\tSpectral Radius:", exp(1/k * log(infNorm/ infinityNorm(xReal))))
 						return
 
 def gaussSeidel(n, b, xReal, eps):
 		k=0
-		x = np.zeros(n, dtype=np.float64)
+		x = np.zeros(n )
 		while True:
 				x[0] = 1/2*(x[1] + b[0])
 				for i in range(1, n-1):
@@ -39,12 +38,12 @@ def gaussSeidel(n, b, xReal, eps):
 				if infNorm < eps:
 						print("\tGauss-Seidel:")
 						print("\t\tK:", k)
-						print("\t\tSpectral Radius:", exp(1/k * log(infNorm/ infinityNorm(xReal), 2)))
+						print("\t\tSpectral Radius:", exp(1/k * log(infNorm/ infinityNorm(xReal))))
 						return
 
 def sor(n, b, xReal, w, eps):
 		k=0
-		x = np.zeros(n, dtype=np.float64)
+		x = np.zeros(n)
 		while True:
 				x[0] = (1-w) * x[0] + w/2*(x[1] + b[0])
 				for i in range(1, n-1):
@@ -55,26 +54,24 @@ def sor(n, b, xReal, w, eps):
 				if infNorm < eps:
 						print("\tSOR:")
 						print("\t\tK:", k)
-						print("\t\tSpectral Radius:", exp(1/k * log(infNorm/ infinityNorm(xReal), 2)))
+						print("\t\tSpectral Radius:", exp(1/k * log(infNorm/ infinityNorm(xReal))))
 						return
 
 def infinityNorm(x):
 		maxVal = -1
 		for val in x:
-				if abs(val) > maxVal:
-						maxVal = abs(val)
+				maxVal = max(maxVal,abs(val))
 		return maxVal
 
 if __name__ == "__main__":
 		nList = [25, 50, 100, 200]
 		wList = [1.78486, 1.88402, 1.93968, 1.96922]
-		eps = 10e-6
+		eps = 10**(-6)
 		for n, w in zip(nList, wList):
 				print("n =", n)
 				h = 1/(n + 1)
-				x0 = np.zeros(n, dtype=np.float64)
-				xReal = np.zeros(n, dtype=np.float64)
-				b = np.zeros(n, dtype=np.float64)
+				xReal = np.zeros(n)
+				b = np.zeros(n)
 					
 				#generating real solutions for this size matrix and the b vector
 				for i in range(0, n):
