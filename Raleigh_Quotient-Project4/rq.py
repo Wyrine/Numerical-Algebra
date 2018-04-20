4b4b59f3258f15677e81c5f5c477e4a16ed7bb43
Wyrine <kashahat@gmail.com>
2018-04-19 21:38:45 -0400

    finished rq.py

--- Raleigh_Quotient-Project4/rq.py
+++ Raleigh_Quotient-Project4/rq.py
@@ -6,6 +6,14 @@
 # Description: Comparison of Stabilized Power Method vs. Raleigh Quotient Iterations
 #								To Approximate Eigenvalues and Vectors
 
+
+# Output of running:
+#Stabilized Power Method:
+#	K = 28 , Lambda = 2.9999805022117965 , eigenVector = [-0.14286309  1.         -0.18366766]
+#Raleigh Quotient Iteration:
+#	K = 13 , Lambda = 3.0000000000011577 , eigenVector = [ 0.14285714 -1.          0.18367347]
+# Therefore, Raleigh Quotient Iteration converges to the eigenpair faster than the Power Method 
+
 from sys import exit
 import numpy as np
 
@@ -42,13 +50,15 @@ def RQ_Iteration(A, q, eps=1e-5):
 				q = np.matmul(np.linalg.inv(B), np.array(q))/sig
 				rq = raleighQuotient(A,q)
 				if abs(rq - mu) <= eps:
-						return it, rq, q/np.linalg.norm(q, 2)
+						return it, rq, q/infNorm(q)
 		
 def main():
 		A = np.array([[-149, -50, -154], [537, 180, 546], [-27, -9, -25]])
 		x0 = np.array([1,1,1]).transpose()
-		print(stabilizedPowerMethod(np.array(A), np.array(x0)))
-		print(RQ_Iteration(np.array(A),np.array(x0)))
+		spm = stabilizedPowerMethod(np.array(A), np.array(x0))
+		rqi = RQ_Iteration(A,x0)
+		print("Stabilized Power Method:\n\tK =", spm[0], ", Lambda =", spm[1], ", eigenVector =", spm[2])
+		print("Raleigh Quotient Iteration:\n\tK =", rqi[0], ", Lambda =", rqi[1], ", eigenVector =", rqi[2])
 		return 0
 
 if __name__ == "__main__":
